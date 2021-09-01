543edc0e5974609c9a21247a22024ec19c40de52
Triantafullenia-Doumani <Triantafulleniad@gmail.com>
2021-09-01 16:12:56 +0200

    if form.isvalid()

--- webshop/views.py
+++ webshop/views.py
@@ -10,49 +10,44 @@ def payment(request, template_name='payment.html'):
 
     if request.method == 'POST':
         form = paymentForm(request.POST)
+        if form.is_valid():
+            sender = User.objects.get(username='webshop')
+            recipient_UWP = form.cleaned_data['address']
 
-        sender = User.objects.get(username='webshop')
-        recipient_UWP = form.cleaned_data['address']
+            xml_type = 'invoice'
 
-        xml_type = 'invoice'
+            via = request.POST['via']
+            if via=='AS4':
+                peppol_classic = False
+            else:
+                peppol_classic = True
 
-        via = request.POST['via']
-        if via=='AS4':
-            peppol_classic = False
-        else:
-            peppol_classic = True
-
-        try:
-            recipient = Activation.objects.get(webID=recipient_UWP)
-            recipient_username = recipient.user.username
-            recipient = User.objects.get(username=recipient_username)
-        except ObjectDoesNotExist:
             try:
-                recipient = Activation.objects.get(peppolID=recipient_UWP)
+                recipient = Activation.objects.get(webID=recipient_UWP)
                 recipient_username = recipient.user.username
                 recipient = User.objects.get(username=recipient_username)
             except ObjectDoesNotExist:
                 try:
-                    recipient = User.objects.get(username=recipient_UWP)
-                    recipient_username = recipient.username
-                except ObjectDoesNotExist as e:
-                    ctx["errors"] = ["%s" % e]
-                    return render(request, template_name, ctx )
+                    recipient = Activation.objects.get(peppolID=recipient_UWP)
+                    recipient_username = recipient.user.username
+                    recipient = User.objects.get(username=recipient_username)
+                except ObjectDoesNotExist:
+                    try:
+                        recipient = User.objects.get(username=recipient_UWP)
+                        recipient_username = recipient.username
+                    except ObjectDoesNotExist as e:
+                        ctx["errors"] = ["%s" % e]
+                        return render(request, template_name, ctx )
 
-        if  sender.username == recipient_username:
-            messages.info(request, _(u"Why are you talking to yourself?"))
-            return render(request, template_name, ctx)
-        else:
-            if form.is_valid():
-                recipient = User.objects.get(pk=recipient.pk)
-                form.save(sender=request.user , recipient=recipient , xml_type=xml_type, peppol_classic = peppol_classic)
-                messages.info(request, _(u"Message successfully sent."))
-                if success_url is None:
-                    success_url = reverse_lazy('django_messages:messages_outbox')
-                if 'next' in request.GET:
-                    success_url = request.GET['next']
+            recipient = User.objects.get(pk=recipient.pk)
+            form.save(sender=request.user , recipient=recipient , xml_type=xml_type, peppol_classic = peppol_classic)
+            messages.info(request, _(u"Message successfully sent."))
+            if success_url is None:
+                success_url = reverse_lazy('django_messages:messages_outbox')
+            if 'next' in request.GET:
+                success_url = request.GET['next']
 
-                return HttpResponseRedirect(success_url)
+            return HttpResponseRedirect(success_url)
     else:
         form = paymentForm()
 
