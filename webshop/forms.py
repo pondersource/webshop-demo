f29824ae54537b615fa968e27df5b112ac33802d
Triantafullenia-Doumani <Triantafulleniad@gmail.com>
2021-09-02 12:12:40 +0200

    explain what address means

--- webshop/forms.py
+++ webshop/forms.py
@@ -1,7 +1,7 @@
-from django import forms      
+from django import forms
 
 from django.utils.translation import gettext_lazy as _
 
 class paymentForm(forms.Form):
 
-    address =  forms.CharField(label=_('Address '))
+    address =  forms.CharField(label=_('Address ') , widget=forms.TextInput(attrs={'placeholder': 'WebID-PeppolID-Email'}))
